2e47d241c07b60456b18da20799890d85b12e7e0
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-03-24 15:03:33 +0700

    Menggunakan perulangan for untuk menyimpan data pda array

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,5 +20,15 @@ void inpu()
         count << "\nArray yang anda masukan maksimal 20 Elemen.\n";
     }
     }
+    count << endl;
+    count << "=================" << endl;
+    count << "Masukan Elemen Array" << endl;
+    count << "=================" << endl;
     
+    for (int i = 0; i < n; i++)
+    {
+    count << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+    }
+     
 }
